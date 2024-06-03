44bc7527dd8aadd23d5d28a08ff212251374cd7b
Neil Horman <nhorman@openssl.org>
2024-06-03 13:28:14 -0400

    Adding initial parameter file

---
+++ projeqtor/initial-parameters.php
@@ -0,0 +1,40 @@
+<?php 
+// =======================================================================================
+// Automatically generated parameter file
+// =======================================================================================
+$paramDbType = 'mysql';
+$paramDbHost = 'mysql_projeqtor';
+$paramDbPort = '3306';
+$paramDbUser = 'admin';
+$paramDbPassword = 'admin';
+$paramDbName = 'projeqtor';
+$paramDbDisplayName = 'ProjeQtOr';
+$paramDbPrefix = '';
+$SslKey = '';
+$SslCert = '';
+$SslCa = '';
+$paramLdap_allow_login = 'false';
+$paramLdap_base_dn = 'dc=mydomain,dc=com';
+$paramLdap_host = 'localhost';
+$paramLdap_port = '389';
+$paramLdap_version = '3';
+$paramLdap_search_user = 'cn=Manager,dc=mydomain,dc=com';
+$paramLdap_search_pass = 'secret';
+$paramLdap_user_filter = 'uid=%USERNAME%';
+$paramMailSmtpServer = 'localhost';
+$paramMailSmtpPort = '25';
+$paramMailSendmailPath = '';
+$paramPasswordMinLength = '5';
+$paramDefaultLocale = 'en';
+$paramDefaultTimezone = 'America/New_York';
+$currency = '$';
+$currencyPosition = 'before';
+$paramAttachmentDirectory = '../files/attach/';
+$documentRoot = '../files/documents/';
+$paramAttachmentMaxSize = '10485760';
+$AttachmentMaxSizeMail = '2097152';
+$paramReportTempDirectory = '../files/report/';
+$paramMemoryLimitForPDF = '512';
+$logFile = '../files/logs/projeqtor_${date}.log';
+$logLevel = '2';
+
